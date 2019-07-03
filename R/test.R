3ed105c12f76f979cc04ee2bda2ec40fa85fbecc
ROBIN Yoann <yoann.robin.k@gmail.com>
2019-07-03 15:42:02 +0200

    --

--- R/test.R
+++ R/test.R
@@ -55,12 +55,68 @@ test_normal = function() ##{{{
 }
 ##}}}
 
+test_gev = function() ##{{{
+{
+	size  = 2500
+	t = base::seq( 0 , 1 , length = size )
+	X0 = t^2
+	X2 = base::seq( -1 , 1 , length = size )
+	loc   = 0.5 + 1.5 * X0
+	scale = 0.1 + 0.1 * X0
+	shape = 0.3 * X2
+	
+	Y = SDFC::rgev( n = size , loc = loc , scale = scale , shape = shape )
+	
+	gev = GEVLaw$new( method = "MLE" , n_bootstrap = 10 )
+	gev$fit( Y , loc_cov = X0  , scale_cov = X0    , shape_cov = X2 )
+	#gev$fit( Y , floc    = loc , scale_cov = X0    , shape_cov = X2 )
+	#gev$fit( Y , loc_cov = X0  , fscale    = scale , shape_cov = X2 )
+	#gev$fit( Y , loc_cov = X0  , scale_cov = X0    , fshape    = shape )
+	
+	graphics::par( mfrow = base::c( 2 , 2 ) )
+	
+	plot( t , Y , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+	
+	plot( loc , gev$loc , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+	
+	plot( scale , gev$scale , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+	
+	plot( shape , gev$shape , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+	
+}
+##}}}
 
 
 ##########
 ## main ##
 ##########
 
-test_normal()
+#test_normal()
+#test_gev()
+
+size  = 2500
+#t = base::seq( 0 , 1 , length = size )
+#X0 = t^2
+#X2 = base::seq( -1 , 1 , length = size )
+#loc   = 0.5 + 1.5 * X0
+#scale = 0.1 + 0.1 * X0
+#shape = 0.3 * X2
+loc   = base::rep( 0. , size )
+scale = base::rep( 1. , size )
+shape = base::rep( 0. , size )
+
+Y = rgpd( n = size , loc = loc , scale = scale , shape = shape )
+
+gpd = GPDLaw$new( method = "moments" )
+gpd$fit( Y , loc = loc )
 
 
+graphics::par( mfrow = base::c( 2 , 2 ) )
+
+plot( t , Y , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+
+plot( loc , gpd$loc , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+
+plot( scale , gpd$scale , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )
+
+plot( shape , gpd$shape , col = grDevices::rgb( 0 , 0 , 1 , 0.5 ) , type = "p" )

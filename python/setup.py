6fad05b775f1595ada324fbce19b9e07444112d9
ROBIN Yoann <yoann.robin.k@gmail.com>
2019-08-26 12:11:24 +0200

    version 0.5.0b0

--- python/setup.py
+++ python/setup.py
@@ -243,7 +243,7 @@ list_packages = [
 setup(
 	name = "SDFC" ,
 	description = "Statistical Distribution Fit with Covariates" ,
-	version = "0.5.0a3" ,
+	version = "0.5.0b0" ,
 	author = "Yoann Robin" ,
 	author_email = "yoann.robin.k@gmail.com" ,
 	license = "CeCILL-C" ,

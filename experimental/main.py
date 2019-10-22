ff1372b1cd952871300b55e729067ada13c09687
ROBIN Yoann <yoann.robin.k@gmail.com>
2019-10-22 08:32:19 +0200

    ...

--- experimental/main.py
+++ experimental/main.py
@@ -113,8 +113,8 @@ def test_gev():##{{{
 	Y = sc.genextreme.rvs( loc = loc , scale = scale , c = - shape )
 	
 	Law = sd.GEV
-	law = Law( method = "mle" , n_bootstrap = 100 )
-	law.fit( Y , c_loc = X_loc , c_scale = X_scale , c_shape = X_shape )
+	law = Law( method = "lmoments" , n_bootstrap = 0 )
+	law.fit( Y )
 	print(law)
 ##}}}
 

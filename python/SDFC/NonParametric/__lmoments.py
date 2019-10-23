4b8345a11384a7eef69a3d32b6fabea5b01bebc0
ROBIN Yoann <yoann.robin.k@gmail.com>
2019-10-23 09:13:14 +0200

    Add fit with covariates, experimental and not reviewed

--- python/SDFC/NonParametric/__lmoments.py
+++ python/SDFC/NonParametric/__lmoments.py
@@ -88,63 +88,78 @@
 
 import numpy         as np
 import scipy.special as scs
+from SDFC.NonParametric.__quantile import quantile
 
 
 ###############
 ## Functions ##
 ###############
 
-def lmoments( X , order = None ):##{{{
+def _lmoments_stationary( Y ):##{{{
+	Ys = np.sort(Y.squeeze())
+	lmom = np.zeros(4)
+	
+	## Order 2
+	C0 = scs.binom( range( Y.size ) , 1 )
+	C1 = scs.binom( range( Y.size - 1 , -1 , -1 ) , 1 )
+	
+	## Order 3
+	C2 = scs.binom( range( Y.size ) , 2 )
+	C3 = scs.binom( range( Y.size - 1 , -1 , -1 ) , 2 )
+	
+	## Order 4
+	C4 = scs.binom( range( Y.size ) , 3 )
+	C5 = scs.binom( range( Y.size - 1 , -1 , -1 ) , 3 )
+	
+	
+	lmom[0] = np.mean(Ys)
+	lmom[1] = np.sum( ( C0 - C1 ) * Ys ) / ( 2 * scs.binom( Y.size , 2 ) )
+	lmom[2] = np.sum( ( C2 - 2 * C0 * C1 + C3 ) * Ys ) / ( 3 * scs.binom( Y.size , 3 ) )
+	lmom[3] = np.sum( (C4 - 3 * C2 * C1 + 3 * C0 * C3 - C5 ) * Ys ) / ( 4 * scs.binom( Y.size , 4 ) )
+	
+	return lmom
+##}}}
+
+def lmoments( Y , c_Y = None , order = None , lq = np.arange( 0.05 , 0.96 , 0.01 ) ):##{{{
 	"""
 		SDFC.NonParametric.lmoments
 		===========================
 		
-		Estimate the lmoments of orders 1 to 4
+		Estimate the lmoments of orders 1 to 4. If a covariate is given, a quantile regression is performed
+		and the instantaneous L-Moments are estimated from the quantile fitted.
 		
 		Parameters
 		----------
-		X     : np.array
+		Y     : np.array
 			Dataset to fit the lmoments
+		c_Y   : np.array or None
+			Covariate
 		order : integer, list of integer or None
 			Integers between 1 and 4
-		
+		lq    : np.array
+			Quantiles for quantile regression, only used if a covariate is given. Default is np.arange(0.05,0.96,0.01)
 		
 		Returns
 		-------
 		The lmoments.
 	"""
-	Xs = np.sort(X.squeeze())
 	
+	order = order if order is None else np.array( [order] , dtype = np.int ).squeeze() - 1
 	
-	lmom = [np.nan,np.nan,np.nan,np.nan]
-	
-	## Order 1
-	lmom[0] = np.mean(Xs)
-	
-	## Order 2
-	C0 = scs.binom( range( X.size ) , 1 )
-	C1 = scs.binom( range( X.size - 1 , -1 , -1 ) , 1 )
-	lmom[1] = np.sum( ( C0 - C1 ) * Xs ) / ( 2 * scs.binom( X.size , 2 ) )
-	
-	## Order 3
-	C2 = scs.binom( range( X.size ) , 2 )
-	C3 = scs.binom( range( X.size - 1 , -1 , -1 ) , 2 )
-	lmom[2] = np.sum( ( C2 - 2 * C0 * C1 + C3 ) * Xs ) / ( 3 * scs.binom( X.size , 3 ) )
-	
-	## Order 4
-	C4 = scs.binom( range( X.size ) , 3 )
-	C5 = scs.binom( range(X.size - 1 , -1 , -1 ) , 3 )
-	lmom[3] = np.sum( (C4 - 3 * C2 * C1 + 3 * C0 * C3 - C5 ) * Xs ) / ( 4 * scs.binom( X.size , 4 ) )
-	
-	
-	if order is None:
-		return lmom
-	
-	out = [lmom[i-1] for i in order]
-	
-	return out
+	if c_Y is None:
+		lmom = _lmoments_stationary(Y)
+		return lmom if order is None else lmom[order]
+	else:
+		Y = Y.reshape(-1,1)
+		if c_Y.ndim == 1: c_Y = c_Y.reshape(-1,1)
+		Yq = quantile( Y , lq , c_Y )
+		lmom = np.zeros((Y.size,4))
+		for i in range(Y.size):
+			lmom[i,:] = _lmoments_stationary(Yq[i,:])
+		if order is None:
+			return lmom
+		else:
+			return lmom[:,order]
 ##}}}
-
-
 
 

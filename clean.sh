d4846da94f85ea6ac666b14939cff2f90709340a
yoann robin <yoann.robin.k@gmail.com>
2020-02-08 07:18:07 +0100

    Add file to clean compilation files

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+## Delete python temporary files
+rm -rf python/SDFC.egg*
+rm -rf python/build
+rm -rf python/dist
+rm -rf python/tmp
+rm -rf python/var
+
+
+## Delete R temporary files
+rm -f R/SDFC/NAMESPACE
+rm -f R/SDFC/man/*.Rd
+rm -f R/*.tar.gz

807a8719ba2838febe502ed5837081dd9e97e28e
= <yoann.robin.k@gmail.com>
2020-10-05 16:12:25 +0200

    Add experimental file for new interface

---
+++ tests/experimental.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+
+#############################
+## Yoann Robin             ##
+## yoann.robin.k@gmail.com ##
+#############################
+
+###############
+## Libraries ##
+##{{{
+
+import sys,os
+import pickle as pk
+import multiprocessing as mp
+
+
+## Scientific libraries
+##=====================
+
+import numpy as np
+import scipy.stats as sc
+import scipy.optimize as sco
+import scipy.linalg as scl
+import pandas as pd
+import xarray as xr
+import SDFC as sd
+
+## Plot libraries ##
+##==================
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import CTP.plot as cplt
+
+#mpl.rcParams['font.size'] = 30
+
+##}}}
+###############
+
+###############
+## Fonctions ##
+###############
+
+
+#############
+## Classes ##
+#############
+
+## Global link function
+##=====================
+
+class GlobalLink:##{{{
+	def __init__( self , *args , **kwargs ):
+		pass
+	
+	def transform( self , coef , X ):
+		pass
+	
+	def jacobian( self , coef , X ):
+		pass
+	
+	def pseudo_inverse( self , params , lin_coef , X ):
+		pass
+##}}}
+
+class GEVPrLink(GlobalLink):##{{{
+	def __init__( self , *args , **kwargs ):
+		GlobalLink.__init__( self , *args , **kwargs )
+	
+	def transform( self , coef , X ):
+		E = np.exp( coef[3] / coef[0] * X[:,0] )
+		loc   = coef[0] * E
+		scale = coef[1] * E
+		shape = coef[2] + np.zeros_like(X[:,0])
+		return loc,scale,shape
+	
+	def jacobian( self , coef , X ):
+		E = np.exp( coef[3] / coef[0] * X[:,0] )
+		jac = np.zeros( (3 , 4 , X[:,0].size) )
+		jac[0,0,:] = E - coef[3] * X[:,0] / coef[0] * E
+		jac[1,0,:] = - coef[1] * coef[3] * X[:,0] / coef[0]**2 * E
+		jac[1,1,:] = E
+		jac[2,2,:] = 1
+		jac[0,3,:] = X[:,0] * E
+		jac[1,3,:] = coef[1] * X[:,0] * E / coef[0]
+		
+		return jac
+	
+	def pseudo_inverse( self , params , lin_coef , X ):
+		
+		coef = np.zeros(4)
+		design = np.stack( (np.ones_like(X),X) , -1 ).squeeze()
+		
+		idxloc   = np.isfinite(np.log(params[:,0]))
+		idxscale = np.isfinite(np.log(params[:,1]))
+		resloc   = scl.lstsq( design[idxloc,:]   , np.log(params[idxloc,0]) )
+		resscale = scl.lstsq( design[idxscale,:] , np.log(params[idxscale,1]) )
+		coef[0] = np.exp(resloc[0][0])
+		coef[1] = np.exp(resscale[0][0])
+		coef[2] = params[:,2].mean()
+		
+		alphaloc   = resloc[0][1] * coef[0]
+		alphascale = resscale[0][1] * coef[0]
+		coef[3]    = ( alphaloc + alphascale ) / 2
+		
+		return coef
+##}}}
+
+class TensorLink(GlobalLink):
+	def __init__( self , l_link , *args , **kwargs ):
+		GlobalLink.__init__( self , *args , **kwargs )
+
+
+## Statistical distribution classes
+##=================================
+
+class AbstractLaw:##{{{
+	
+	def __init__( self , method ): ##{{{
+		self._method   = method
+		self._c_global = None
+		self._l_global = None
+	##}}}
+	
+	def fit( self , Y , **kwargs ): ##{{{
+		pass
+	##}}}
+	
+##}}}
+
+class Normal(AbstractLaw):##{{{
+	
+	def __init__( self , method = "MLE" ):##{{{
+		AbstractLaw.__init__( self , method )
+	##}}}
+	
+	## Fit methods
+	##============
+	
+	def _special_fit( self , Y , **kwargs ):##{{{
+		pass
+	##}}}
+	
+	def _init_MLE( self , Y , **kwargs ): ##{{{
+		pass
+	##}}}
+	
+##}}}
+
+
+class GEV:##{{{
+	
+	def __init__( self , link , restart_fit = 0 ):##{{{
+		self.link  = link
+		self.optim = None
+		self.coef_ = None
+		self.restart_fit = restart_fit
+		self.n_restart   = 0
+	##}}}
+	
+	def negloglikelihood( self , coef ): ##{{{
+		self.coef_ = coef
+		loc,scale,shape = self.link.transform( self.coef_ , self._X )
+		return - np.sum( sc.genextreme.logpdf( self._Y , loc = loc , scale = scale , c = -shape ) )
+	##}}}
+
+	def grad_nlll( self , coef ): ##{{{
+		self.coef_ = coef
+		jac = self.link.jacobian( self.coef_ , self._X )
+		loc,scale,shape = self.link.transform( self.coef_ , self._X )
+		
+		
+		Z  = ( self._Y - loc ) / scale
+		ZZ = 1 + shape * Z
+		ZZi = ZZ**( - 1 / shape )
+		kappa = ( 1 + 1 / shape ) / ZZ - ZZi / (shape * ZZ)
+		
+		T0 = - shape * kappa / scale
+		T1 = 1 / scale - shape * Z / scale * kappa
+		T2 = np.log(ZZ) * ( ZZi - 1 ) / shape**2 + Z * kappa
+		
+		
+		
+		
+		jac[0,:] *= T0
+		jac[1,:] *= T1
+		jac[2,:] *= T2
+		
+		return jac.sum( axis = (0,2) )
+	##}}}
+	
+	def init_mle( self ):##{{{
+		sdgev = sd.GEV( method = "lmoments-experimental" )
+		sdgev.fit( self._Y , c_loc = np.exp(self._X) , c_scale = np.exp(self._X) )
+		params = np.stack( (sdgev.loc,sdgev.scale,sdgev.shape) ).squeeze().T
+		minit   = self.link.pseudo_inverse( params , sdgev.coef_ , self._X )
+		mcov    = np.identity(4)
+		
+		## Perturbation if the init point is not well defined
+		init = minit.copy()
+		nit = 0
+		while not np.isfinite(self.negloglikelihood(init)) or not np.isfinite(self.grad_nlll(init).all()):
+			init = np.random.multivariate_normal( mean = minit , cov = mcov )
+			nit += 1
+			if nit % 100 == 0: mcov *= 2
+		
+		self.coef_ = init
+	##}}}
+	
+	def fit( self , Y , X ):##{{{
+		self._Y = Y.squeeze()
+		self._X = X.reshape(-1,1) if X.ndim == 1 else X
+		self.init_mle()
+		self.optim = sco.minimize( self.negloglikelihood , self.coef_ , jac = self.grad_nlll )
+		
+		restart_init = self.restart_fit
+		if not self.optim.success and self.restart_fit > 0:
+			self.restart_fit -= 1
+			self.fit( Y , X )
+		self.n_restart   = restart_init - self.restart_fit
+		self.restart_fit = restart_init
+		
+	##}}}
+##}}}
+
+
+def test_GEV(): ##{{{
+	n_samples = 2000
+	t,X,_,_ = sd.tools.Dataset.covariates( n_samples )
+	X = X.reshape(-1,1)
+	l_global = GEVPrLink()
+	
+	
+	coef_true = np.array([0.5,1.5,-0.2,1.3])
+	loc,scale,shape = l_global.transform( coef_true , X )
+	Y = sc.genextreme.rvs( loc = loc , scale = scale , c = -shape )
+	
+	gev = GEV( l_global , 10 )
+	gev.fit( Y , X )
+	
+	print(coef_true)
+	print(gev.optim.x)
+	print(gev.n_restart)
+##}}}
+
+
+##########
+## main ##
+##########
+
+if __name__ == "__main__":
+	np.seterr( all = "ignore" )
+	
+	## Build data
+	##===========
+	n_samples = 2000
+	t,X_loc,X_scale,_ = sd.tools.Dataset.covariates( n_samples )
+	X_loc   = X_loc.reshape(-1,1)
+	X_scale = X_scale.reshape(-1,1)
+	
+	loc   = 1. + 3 * X_loc
+	scale = np.exp( - 0.5 * X_scale )
+	
+	Y = np.random.normal( loc = loc , scale = scale )
+	
+	## Fit
+	##====
+	norm = Normal()
+	
+	
+	## And plot it
+	##============
+	nrow,ncol = 1,1
+	fig = plt.figure()
+	
+	ax = fig.add_subplot( nrow , ncol , 1 )
+	ax.plot( t , Y )
+	ax.plot( t , X_loc )
+	ax.plot( t , np.exp(-X_scale) )
+	
+	plt.show()
+	
+	print("Done")

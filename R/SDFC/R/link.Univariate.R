a089cc8f1162a5aa5814787f23e1dcda758b44fc
= <yoann.robin.k@gmail.com>
2020-12-11 09:08:39 +0100

    Update doc and add ULExpoential

--- R/SDFC/R/link.Univariate.R
+++ R/SDFC/R/link.Univariate.R
@@ -27,6 +27,9 @@
 #' @details
 #' Just to be inherit
 #'
+#' @importFrom R6 R6Class
+#' @importFrom methods new
+#'
 #' @examples
 #' ##
 #'
@@ -55,7 +58,13 @@ UnivariateLink = R6::R6Class( "UnivariateLink" ,
 #' Identity link function
 #'
 #' @details
-#' Identity link function
+#' Identity link function:
+#' f(x)    = x
+#' f^-1(x) = x
+#' f'(x)   = 1
+#'
+#' @importFrom R6 R6Class
+#' @importFrom methods new
 #'
 #' @examples
 #'
@@ -68,13 +77,19 @@ ULIdentity = R6::R6Class( "ULIdentity" ,
 	
 	public = list(
 	
+	## initialize ##{{{
+	
 	#' @description
     #' Create a new ULIdentity object.
 	#' @return A new `ULIdentity` object.
-	initialize = function(...)
+	initialize = function()
 	{
 		super$initialize()
 	},
+	
+	##}}}
+	
+	## transform ##{{{
 	
 	#' @description
     #' Transform method
@@ -84,6 +99,9 @@ ULIdentity = R6::R6Class( "ULIdentity" ,
 	{
 		return(x)
 	},
+	##}}}
+	
+	## inverse ##{{{
 	
 	#' @description
     #' Inverse method
@@ -93,6 +111,9 @@ ULIdentity = R6::R6Class( "ULIdentity" ,
 	{
 		return(x)
 	},
+	##}}}
+	
+	## jacobian ##{{{
 	
 	#' @description
     #' Jacobian method
@@ -102,10 +123,103 @@ ULIdentity = R6::R6Class( "ULIdentity" ,
 	{
 		return(x)
 	}
+	##}}}
 	
 	)
 )
 
 ##}}}
 
+## ULExponential ##{{{
+
+#' ULExponential 
+#'
+#' @description
+#' Exponential link function
+#'
+#' @details
+#' Exponential link function, usefull to constrain a parameter in [b;+inf[ if
+#' s > 0 or ]-inf;b] if s < 0.
+#' f(x)    = base::exp(s*x) + b
+#' f^-1(x) = base::log(x-b) / s
+#' f'(x)   = s * base::exp(s*x)
+#'
+#' @importFrom R6 R6Class
+#' @importFrom methods new
+#'
+#' @examples
+#'
+#' explink = SDFC::ULExponential$new()
+#'
+#' @export
+ULExponential = R6::R6Class( "ULExponential" ,
+	
+	inherit = UnivariateLink,
+	
+	public = list(
+	
+	## Public attributes ## {{{
+	#' @field b [float] Lower / Upper bound of exponential link
+	b = NULL,
+	#' @field s [float] Speed of exponential link
+	s = NULL,
+	
+	##}}}
+	
+	## initialize ##{{{
+	
+	#' @description
+    #' Create a new ULExponential object.
+	#' @param b [float] Lower / Upper bound of exponential link
+	#' @param s [float] Speed of exponential link
+	#' @return A new `ULExponential` object.
+	initialize = function( b = 0 , s = 1 )
+	{
+		super$initialize()
+		self$b = b
+		self$s = s
+	},
+	
+	##}}}
+	
+	## transform ##{{{
+	
+	#' @description
+    #' Transform method
+    #' @param x [vector] Value to transform
+    #' @return Value transformed
+	transform = function(x)
+	{
+		return( base::exp( self$s * x ) + self$b )
+	},
+	##}}}
+	
+	## inverse ##{{{
+	
+	#' @description
+    #' Inverse method
+    #' @param x [vector] Value to inverse
+    #' @return Value inversed
+	inverse = function(x)
+	{
+		return( base::log( x - self$b ) / self$s )
+	},
+	##}}}
+	
+	## jacobian ##{{{
+	
+	#' @description
+    #' Jacobian method
+    #' @param x [vector] Value to evaluate the jacobian
+    #' @return Jacobian at x
+	jacobian = function(x)
+	{
+		return(self$s * base::exp(self$s * x))
+	}
+	##}}}
+	
+	)
+)
+
+##}}}
 
